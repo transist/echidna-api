6bf8784d60f762ea52b354ecf9f9bbf594c35958
Ricky Ng-Adam <rngadam@gmail.com>
2013-03-05 14:23:22 +0800

    echo server working when used against /socket.io

--- echo-client.js
+++ echo-client.js
@@ -1,11 +1,8 @@
+var options = {};
+
 var socketioclient = require('socket.io-client');
 
-var base = 'http://127.0.0.1:9999';
-
-var uri = process.env.ECHIDNA_ECHO_URI || 'socket.io';
-var options = {
-  //resource: uri + '/socket.io'
-};
+var base = process.env.ECHIDNA_ECHO_URL || 'http://127.0.0.1:9999';
 
 console.log('connecting to ' + base);
 console.log('options ' + JSON.stringify(options));
--- echo-server.js
+++ echo-server.js
@@ -1,15 +1,8 @@
+var options = {};
 
-
-var uri = process.env.ECHIDNA_ECHO_URI || 'socket.io';
-var options = {
-  //resource: uri + '/socket.io'
-};
-
-console.log('starting server with uri ' + uri);
 console.log('options ' + JSON.stringify(options));
-
 var io = require('socket.io').listen(9999, options);
-io.set('log level', 1);
+io.set('log level', 5);
 
 var rootSockets = io.on('connection', function(socket)
 {
